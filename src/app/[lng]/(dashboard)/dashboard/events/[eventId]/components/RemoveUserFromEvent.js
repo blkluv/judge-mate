9d8316b0b4dda5748cc97f2610ef38bc452028bb
kacperszymanski <akacperszymanski@gmail.com>
2023-09-15 18:30:49 +0200

    add remove from userEvents

--- src/app/[lng]/(dashboard)/dashboard/events/[eventId]/components/RemoveUserFromEvent.js
+++ src/app/[lng]/(dashboard)/dashboard/events/[eventId]/components/RemoveUserFromEvent.js
@@ -8,15 +8,16 @@ import {
   doc,
   updateDoc,
   deleteDoc,
-  FieldValue,
+  arrayRemove, // Importuj arrayRemove z Firebase Firestore
 } from "firebase/firestore";
 
 const RemoveUserFromEvent = ({ eventId }) => {
   const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
 
   const handleRemoveUser = async () => {
     if (!username) {
-      alert("Please provide a username.");
+      setError("Please provide a username.");
       return;
     }
 
@@ -27,7 +28,7 @@ const RemoveUserFromEvent = ({ eventId }) => {
       const userSnapshot = await getDocs(q);
 
       if (userSnapshot.empty) {
-        alert("User not found.");
+        setError("User not found.");
         return;
       }
 
@@ -36,22 +37,24 @@ const RemoveUserFromEvent = ({ eventId }) => {
       // 2. Update the event to remove the user role
       const eventRef = doc(db, "events", eventId);
       await updateDoc(eventRef, {
-        [`roles.${userId}`]: FieldValue.delete(),
+        [`roles.${userId}`]: null, // Usunięcie pola z obiektu roles
       });
 
       // 3. Delete the user's events subcollection record for this event
-      const userEventRef = doc(db, `users/${userId}/userEvents`, eventId);
+      const userEventRef = doc(db, `users/${userId}/userEvents/${eventId}`);
       await deleteDoc(userEventRef);
 
+      setError(""); // Wyczyść ewentualny poprzedni błąd
+      setUsername(""); // Wyczyść pole wejściowe
       alert("User successfully removed from the event!");
-      setUsername(""); // Clear the input
     } catch (error) {
       console.error("Error removing user from event:", error);
-      alert(
+      setError(
         "An error occurred while trying to remove the user from the event."
       );
     }
   };
+
   return (
     <div>
       <h2>Remove User from Event</h2>
@@ -67,6 +70,7 @@ const RemoveUserFromEvent = ({ eventId }) => {
         <button type="button" onClick={handleRemoveUser}>
           Remove User
         </button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
     </div>
   );
