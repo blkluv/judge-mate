ea42276ea64531e95590cef2ecc0c20a019a2d35
kacperszymanski <akacperszymanski@gmail.com>
2023-09-18 21:10:44 +0200

    refactor events page

--- src/app/[lng]/(dashboard)/dashboard/events/[eventId]/page.js
+++ src/app/[lng]/(dashboard)/dashboard/events/[eventId]/page.js
@@ -1,56 +1,42 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext, createContext } from "react";
 import { fetchData } from "../../../../../../firebase/firestore/fetchData";
+import { useAuthContext } from "../../../../../../firebase/context/AuthContext";
+import styles from "./page.module.css";
 import AddUserToEvent from "./(users-management)/AddUserToEvent";
 import EventUsers from "./(users-management)/EventUsers";
 import JudgingTableCreator from "./components/JudgingTableCreator";
 import RemoveUserFromEvent from "./(users-management)/RemoveUserFromEvent";
-import JudgeScoring from "./components/JudgeScoring"; // Import JudgeScoring component
+import JudgeScoring from "./components/JudgeScoring";
 import AllParticipantsScoresTable from "./components/AllParticipantsScoresTable";
-import { useAuthContext } from "../../../../../../firebase/context/AuthContext"; // Import the AuthContext
-import styles from "./page.module.css";
+
+const EventContext = createContext();
 
 function Page({ params: { eventId } }) {
-  const router = useRouter();
+  const { user } = useAuthContext();
   const [eventData, setEventData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const { user } = useAuthContext(); // Get user from AuthContext
-
   useEffect(() => {
-    // Ensure router is ready before attempting to access query parameters
-    if (router.isReady) {
-      const { eventId } = router.query;
-      setEventData({ ...eventData, eventId });
-    }
-  }, [router.isReady]);
+    async function fetchEvent() {
+      const { data, error } = await fetchData("events", eventId);
 
-  useEffect(() => {
-    if (eventId) {
-      // Fetch event data
-      async function fetchEvent() {
-        const { data, error } = await fetchData("events", eventId);
-
-        if (data) {
-          setEventData(data);
-        }
-        if (error) {
-          setError(error);
-        }
-
-        setLoading(false);
+      if (data) {
+        setEventData(data);
+      }
+      if (error) {
+        setError(error);
       }
 
-      fetchEvent();
+      setLoading(false);
     }
+
+    if (eventId) fetchEvent();
   }, [eventId]);
 
-  // Check if user is a judge for this event
   const isJudge = eventData?.roles?.[user?.uid] === "judge";
-
-  const isOrganizer = eventData?.roles?.[user?.uid] === "organizer"; // Check if user is an organizer
+  const isOrganizer = eventData?.roles?.[user?.uid] === "organizer";
 
   if (loading) {
     return <div>Loading event...</div>;
@@ -61,30 +47,18 @@ function Page({ params: { eventId } }) {
   }
 
   return (
-    <div>
-      <h1>{eventData?.eventName}</h1>
-      <p>Date: {eventData?.eventDate}</p>
-      <div className={`${styles.usersManagementSection}`}>
-        <h2>Event management</h2>
-        {isOrganizer && <AddUserToEvent eventId={eventId} />}{" "}
-        {isOrganizer && <RemoveUserFromEvent eventId={eventId} />}{" "}
-        {/* Display AddUserToEvent for organizers */}
+    <EventContext.Provider value={eventData}>
+      <div>
+        <h1>{eventData?.eventName}</h1>
+        <p>Date: {eventData?.eventDate}</p>
+        {isOrganizer && <AddUserToEvent eventId={eventId} />}
+        {isOrganizer && <RemoveUserFromEvent eventId={eventId} />}
         <EventUsers eventId={eventId} />
+        {isOrganizer && <JudgingTableCreator eventId={eventId} />}
+        {isJudge && <JudgeScoring eventId={eventId} userId={user.uid} />}
+        <AllParticipantsScoresTable eventId={eventId} />
       </div>
-      {isOrganizer && (
-        <div className={`${styles.tableConfigSection}`}>
-          <h2>Table config</h2>
-          <JudgingTableCreator eventId={eventId} />{" "}
-        </div>
-      )}{" "}
-      {/* Display JudgingTableCreator for organizers */}
-      {isJudge && (
-        <div className={`${styles.usersManagementSection}`}>
-          <JudgeScoring eventId={eventId} userId={user.uid} />{" "}
-        </div>
-      )}{" "}
-      <AllParticipantsScoresTable eventId={eventId} />
-    </div>
+    </EventContext.Provider>
   );
 }
 
