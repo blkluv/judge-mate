0d88ab661452986feb09df70478fb94a9a3d0591
kacperszymanski <akacperszymanski@gmail.com>
2023-10-01 11:28:01 +0200

    user profile

--- src/app/[lng]/(dashboard)/dashboard/user-profile/page.js
+++ src/app/[lng]/(dashboard)/dashboard/user-profile/page.js
@@ -1,14 +1,44 @@
 "use client";
-
+import React, { useState } from "react";
 import EditUserProfile from "./components/EditUserProfile";
 import LogoutButton from "./components/LogoutButton";
 import styles from "./page.module.css";
 
 function Page() {
+  const [selectedOption, setSelectedOption] = useState("Dane osobowe");
+
+  const renderContent = () => {
+    switch (selectedOption) {
+      case "Dane osobowe":
+        return <EditUserProfile />;
+      case "Zamówienia":
+        return <div>Lista Twoich zamówień...</div>;
+      case "Ustawienia newslettera":
+        return <div>Ustawienia subskrypcji newslettera...</div>;
+      case "Pomoc i FAQ":
+        return <div>Odpowiedzi na najczęściej zadawane pytania...</div>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <LogoutButton />
-      <EditUserProfile />
+      <div className={styles.sidebar}>
+        <ul>
+          <li onClick={() => setSelectedOption("Dane osobowe")}>
+            Dane osobowe
+          </li>
+          <li onClick={() => setSelectedOption("Zamówienia")}>Zamówienia</li>
+          <li onClick={() => setSelectedOption("Ustawienia newslettera")}>
+            Ustawienia newslettera
+          </li>
+          <li onClick={() => setSelectedOption("Pomoc i FAQ")}>Pomoc i FAQ</li>
+        </ul>
+      </div>
+      <div className={styles.content}>{renderContent()}</div>
+      {/* <LogoutButton />
+       */}
     </div>
   );
 }
